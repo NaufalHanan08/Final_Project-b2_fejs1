18a6f144d91ea75c542a9f4548b8b05f9ae51677
Naufal Hanan <Hanan080602@gmail.com>
2023-11-26 21:02:43 +0700

    slicing register dan login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   const [navbar, setNavbar] = useState(false);
@@ -34,20 +35,14 @@ function Navbar() {
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <a href="javascript:void(0)">
-              <h2 className="text-2xl font-bold text-white"><span className="text-lime-600 underline">Byte</span>Academy</h2>
+              <h2 className="text-2xl font-bold text-white">
+                <span className="text-lime-600 underline">Byte</span>Academy
+              </h2>
             </a>
             <div className="md:hidden">
-              <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                onClick={() => setNavbar(!navbar)}
-              >
+              <button className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border" onClick={() => setNavbar(!navbar)}>
                 {navbar ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" viewBox="0 0 20 20" fill="currentColor">
                     <path
                       fillRule="evenodd"
                       d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
@@ -55,19 +50,8 @@ function Navbar() {
                     />
                   </svg>
                 ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                   </svg>
                 )}
               </button>
@@ -75,11 +59,7 @@ function Navbar() {
           </div>
         </div>
         <div>
-          <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? "block" : "hidden"
-            }`}
-          >
+          <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? 'block' : 'hidden'}`}>
             <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 lg:text-md text-sm">
               <li className="text-slate-400 hover:text-lime-600 transition-all">
                 <a href="javascript:void(0)">HOME</a>
@@ -96,12 +76,12 @@ function Navbar() {
                   <input type="text" placeholder="Search here..." className="outline-none py-2 px-2 flex-2" />
                   <button className="border border-lime-600 text-lime-600 py-2 px-3">Search</button>
                 </ul>
-                <li>
+                <Link to="/Login">
                   <button className="w-24 bg-lime-800 border border-lime-800 hover:bg-transparent transition-all text-white text-sm py-2">LOGIN</button>
-                </li>
-                <li className="">
+                </Link>
+                <Link to="/Register">
                   <button className="w-24 bg-lime-800 border border-lime-800 hover:bg-transparent transition-all text-white text-sm py-2">REGISTER</button>
-                </li>
+                </Link>
               </ul>
             </ul>
           </div>
--- src/pages/authentication/RegisterPage.jsx
+++ src/pages/authentication/RegisterPage.jsx
@@ -1,9 +1,66 @@
+import { Card, Input, Button, Typography } from '@material-tailwind/react';
+import { Link } from 'react-router-dom';
 
-
-function RegisterPage() {
+export function RegisterPage() {
   return (
-    <div>RegisterPage</div>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <Card color="transparent" shadow={false}>
+        <Typography variant="h4" color="blue-gray">
+          Sign Up
+        </Typography>
+        <Typography color="gray" className="mt-1 font-normal">
+          Nice to meet you! Enter your details to register.
+        </Typography>
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Name
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="Your Name"
+              className="border-t-blue-gray-200 focus:border-t-gray-900"
+              labelProps={{
+                className: 'before:content-none after:content-none',
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Email
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className="border-t-blue-gray-200 focus:border-t-gray-900"
+              labelProps={{
+                className: 'before:content-none after:content-none',
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Password
+            </Typography>
+            <Input
+              type="password"
+              size="lg"
+              placeholder="********"
+              className="border-t-blue-gray-200 focus:border-t-gray-900"
+              labelProps={{
+                className: 'before:content-none after:content-none',
+              }}
+            />
+          </div>
+          <Button className="bg-blue-600 hover:bg-blue-700 text-white mt-6" fullWidth>
+            Sign up
+          </Button>
+          <Typography color="gray" className="mt-4 text-center font-normal">
+            Already have an account?{' '}
+            <Link to="/login" className="font-medium text-gray-900">
+              Sign In
+            </Link>
+          </Typography>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
