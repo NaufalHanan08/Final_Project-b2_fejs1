e21ccd0be757ea0c18ec630704eefffc34e919e4
zaqihdyt13 <sakiihdyt13@gmail.com>
2023-11-26 12:36:01 +0700

    memperbarui beberapa tampilan

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/authentication/LoginPage"
 import RegisterPage from "./pages/authentication/RegisterPage"
+import SearchPage from "./pages/SearchPage"
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage/>} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/search" element={<SearchPage/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import heroimg from "../assets/heroimg.png"
+
+function Hero() {
+  return (
+    <div className="w-full bg-teal-600">
+      <div className="bg-hero w-full md:h-fit md:block flex flex-col-reverse items-center md:px-20 px-5">
+        <div className="hero-content  md:w-2/4 w-full md:py-28 py-5 md:mb-0 mb-20 md:mt-10 mt-0 overflow-hidden md:block flex flex-col items-center">
+          <h1 className="md:text-4xl text-3xl py-2 px-3 pb-4 rounded-md font-bold mb-3 text-white bg-gray-800 w-fit"><span className="text-teal-600 underline">Byte</span>Academy</h1>
+          <h2 className="md:text-xl text-md text-white py-6 md:text-start text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+            reiciendis dolorum vitae placeat doloremque ducimus.
+          </h2>
+          <div>
+            <button className="text-white font-semibold bg-teal-900 border border-teal-900 hover:bg-teal-600 hover:border-teal-900 hover:text-teal-900 transition-all duration-300 rounded-lg py-2 px-3 mt-5">Daftar Sekarang</button>
+          </div>
+        </div>
+        <div>
+          <img src={heroimg} alt="hero-img" className="md:h-80 h-3/4 md:absolute static md:mt-0 mt-20 right-20 bottom-32  bg-teal-600 border-b-4 border-teal-900 rounded-full p-3 shadow-xl shadow-teal-900" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
