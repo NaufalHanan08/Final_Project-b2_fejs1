c8b6fd20d3a5e2fae9d1c6e86ccc7d9a52d39c70
adamnanwar <adamnanwar1201@gmail.com>
2023-12-01 20:11:14 +0700

    merge fe

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,46 +1,45 @@
 function Benefits() {
-    return (
-      <div className="benefits-img w-full flex justify-end">
-        <div className="benefits-content md:w-2/4 w-full py-28 px-10 overflow-hidden">
-          <h3 className="bg-gray-700 text-white font-bold ps-5 shadow-md">
-            LEARN ANYTHING
-          </h3>
-          <h1 className="text-black font-bold text-4xl mb-14 overflow-hidden">
-            Benefits About Online Learning Expertise
-          </h1>
-          <div className="flex flex-col gap-3">
-            <a href="" className="bg-slate-100 hover:bg-gray-700 p-10 text-black hover:text-white transition-all duration-300">
-              <h2 className="text-2xl mb-3">Online Courses</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
-                repellat hic, neque, asperiores voluptatibus expedita cupiditate
-                quo aliquid atque, eum nulla dolorum eius ducimus alias architecto
-                adipisci odio omnis. Expedita!
-              </p>
-            </a>
-            <a href="" className="bg-slate-100 hover:bg-gray-700 p-10 text-black hover:text-white transition-all duration-300">
-              <h2 className="text-2xl mb-3">Earn A Certificates</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
-                error, architecto voluptatibus ipsa tenetur unde blanditiis
-                aliquam animi suscipit dolorem temporibus magnam et, consequatur
-                facilis rerum nostrum. Neque, provident architecto?
-              </p>
-            </a>
-            <a href="" className="bg-slate-100 hover:bg-gray-700 p-10 text-black hover:text-white transition-all duration-300">
-              <h2 className="text-2xl mb-3">Learn with Expert</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Asperiores et, repudiandae facilis quidem assumenda accusantium
-                reiciendis placeat error ratione iure consequuntur eveniet
-                praesentium ipsa rerum est. Aliquam magnam nostrum impedit.
-              </p>
-            </a>
-          </div>
+  return (
+    <div className="benefits-img w-full flex justify-end">
+      <div className="benefits-content md:w-2/4 w-full py-28 px-10 overflow-hidden">
+        <h3 className="bg-gray-800 text-white font-bold ps-5 shadow-md">
+          <span className="text-teal-600">LEARN</span> ANYTHING
+        </h3>
+        <h1 className="text-black font-bold text-4xl mb-14 overflow-hidden">
+          Benefits About Online Learning Expertise
+        </h1>
+        <div className="flex flex-col gap-3">
+          <a href="" className="bg-slate-100 hover:bg-gray-800 p-10 text-black hover:text-white transition-all duration-300">
+            <h2 className="text-2xl font-bold mb-3">Online Courses</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              repellat hic, neque, asperiores voluptatibus expedita cupiditate
+              quo aliquid atque, eum nulla dolorum eius ducimus alias architecto
+              adipisci odio omnis. Expedita!
+            </p>
+          </a>
+          <a href="" className="bg-slate-100 hover:bg-gray-800 p-10 text-black hover:text-white transition-all duration-300">
+            <h2 className="text-2xl font-bold mb-3">Earn A Certificates</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
+              error, architecto voluptatibus ipsa tenetur unde blanditiis
+              aliquam animi suscipit dolorem temporibus magnam et, consequatur
+              facilis rerum nostrum. Neque, provident architecto?
+            </p>
+          </a>
+          <a href="" className="bg-slate-100 hover:bg-gray-800 p-10 text-black hover:text-white transition-all duration-300">
+            <h2 className="text-2xl font-bold mb-3">Learn with Expert</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores et, repudiandae facilis quidem assumenda accusantium
+              reiciendis placeat error ratione iure consequuntur eveniet
+              praesentium ipsa rerum est. Aliquam magnam nostrum impedit.
+            </p>
+          </a>
         </div>
       </div>
-    );
-  }
-  
-  export default Benefits;
-  
+    </div>
+  );
+}
+
+export default Benefits;
--- src/pages/authentication/LoginPage.jsx
+++ src/pages/authentication/LoginPage.jsx
@@ -1,9 +1,58 @@
+import { Card, Input, Button, Typography } from '@material-tailwind/react';
+import { Link } from 'react-router-dom';
 
-
-function LoginPage() {
+export function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <Card color="transparent" shadow={false}>
+        <Typography variant="h4" color="blue-gray">
+          Sign In
+        </Typography>
+        <Typography color="gray" className="mt-1 font-normal">
+          Welcome back! Log in to your account.
+        </Typography>
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Email
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: 'before:content-none after:content-none',
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Password
+            </Typography>
+            <Input
+              type="password"
+              size="lg"
+              placeholder="********"
+              className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: 'before:content-none after:content-none',
+              }}
+            />
+          </div>
+          <Link to="/forgot-password" className="text-blue-600 hover:underline mb-4">
+            Forgot Password?
+          </Link>
+          <Button className="bg-blue-600 hover:bg-blue-700 text-white mt-6" fullWidth>
+            Sign In
+          </Button>
+          <Typography color="gray" className="mt-4 text-center font-normal">
+            Don't have an account?{' '}
+            <Link to="/register" className="font-medium text-gray-900">
+              Sign Up
+            </Link>
+          </Typography>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
