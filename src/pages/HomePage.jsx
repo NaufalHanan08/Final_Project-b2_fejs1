637efd1156c71f7c62182bd45dabe9e6e277da1a
Naufal Hanan <Hanan080602@gmail.com>
2023-12-12 20:21:27 +0700

    melakukan merge dari mas zaqi lalu memperbaiki bagian homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,19 @@
+import Navbar from '../components/Navbar';
 import CourseList from '../components/courselist/CourseList';
 import Benefits from '../components/Benefits';
 import Hero from '../components/Hero';
+import Footer from '../components/Footer';
 import RegisterBanner from '../components/RegisterBanner';
 
 function HomePage() {
   return (
     <>
+      <Navbar />
       <Hero />
       <CourseList />
       <Benefits />
       <RegisterBanner />
+      <Footer />
     </>
   );
 }
